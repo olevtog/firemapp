a776ac368558fd99b73fa1366680acd17f97c34c
Defossez Gautier <gautier.defossez.etu@univ-lille.fr>
2018-11-29 17:43:44 +0100

    affichage de la map

---
+++ app/src/main/java/com/valette/defossez/firemapp/MapsActivity.kt
@@ -0,0 +1,64 @@
+package com.valette.defossez.firemapp
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.BitmapDescriptorFactory
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
+
+
+class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
+
+    private lateinit var mMap: GoogleMap
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_maps)
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        val mapFragment = supportFragmentManager
+                .findFragmentById(R.id.map) as SupportMapFragment
+        mapFragment.getMapAsync(this)
+    }
+
+    /**
+     * Manipulates the map once available.
+     * This callback is triggered when the map is ready to be used.
+     * This is where we can add markers or lines, add listeners or move the camera. In this case,
+     * we just add a marker near Sydney, Australia.
+     * If Google Play services is not installed on the device, the user will be prompted to install
+     * it inside the SupportMapFragment. This method will only be triggered once the user has
+     * installed Google Play services and returned to the app.
+     */
+    override fun onMapReady(googleMap: GoogleMap) {
+        mMap = googleMap
+
+        // pour supprimer la barre en bas pour afficher la direction et un lien vers maps
+        mMap.uiSettings.isMapToolbarEnabled = false
+
+
+        // Add a marker in Sydney and move the camera
+        val sydney = LatLng(-34.0, 151.0)
+
+        //mMap.addMarker(MarkerOptions().position(sydney))
+        //mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+
+
+
+        mMap.addMarker(MarkerOptions()
+                .position(LatLng(-35.0, 150.0))
+                .title("My Spot")
+                .snippet("This is my spot!")
+                .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_AZURE)))
+
+        mMap.addMarker(MarkerOptions()
+                .position(sydney)
+                .title("My Spot")
+                .snippet("This is my spot!")
+                .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_AZURE)))
+
+    }
+
+}
