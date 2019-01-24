7814e9b78ce4ad0a4029ea1453a1be1547922aa8
gautier <gautier-59@hotmail.fr>
2019-01-24 21:38:25 +0100

    ajout slide up au dessus de la map

--- app/src/main/java/com/valette/defossez/firemapp/MapsActivity.kt
+++ app/src/main/java/com/valette/defossez/firemapp/MapsActivity.kt
@@ -2,15 +2,20 @@ package com.valette.defossez.firemapp
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.widget.Toast
+import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.BitmapDescriptorFactory
 import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.Marker
 import com.google.android.gms.maps.model.MarkerOptions
+import com.sothree.slidinguppanel.SlidingUpPanelLayout
+import kotlinx.android.synthetic.main.activity_maps.*
 
 
-class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
+class MapsActivity : AppCompatActivity(), OnMapReadyCallback, GoogleMap.OnMarkerClickListener {
 
     private lateinit var mMap: GoogleMap
 
@@ -21,6 +26,14 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
         val mapFragment = supportFragmentManager
                 .findFragmentById(R.id.map) as SupportMapFragment
         mapFragment.getMapAsync(this)
+
+        sliding_layout.anchorPoint = .3f
+
+
+    }
+
+    override fun onMarkerClick(marker: Marker?): Boolean {
+        return true
     }
 
     /**
@@ -43,14 +56,12 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
         val sydney = LatLng(-34.0, 151.0)
 
         //mMap.addMarker(MarkerOptions().position(sydney))
-        //mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
-
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
 
 
         mMap.addMarker(MarkerOptions()
                 .position(LatLng(-35.0, 150.0))
                 .title("My Spot")
-                .snippet("This is my spot!")
                 .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_AZURE)))
 
         mMap.addMarker(MarkerOptions()
