bd7b602522659a6fb3bd45cbd0d32bdb01ec7645
hvalette <zongo512 @gmail.com>
2019-02-14 17:04:29 +0100

    fix override getAll

--- app/src/main/java/com/valette/defossez/firemapp/controller/FireworksController.kt
+++ app/src/main/java/com/valette/defossez/firemapp/controller/FireworksController.kt
@@ -1,30 +1,29 @@
 package com.valette.defossez.firemapp.controller
 
-import com.google.firebase.database.FirebaseDatabase
-import com.valette.defossez.firemapp.entity.Firework
-import com.google.firebase.FirebaseError
 import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.FirebaseDatabase
 import com.google.firebase.database.ValueEventListener
-
+import com.valette.defossez.firemapp.entity.Firework
 
 
 class FireworksController {
 
     private val database = FirebaseDatabase.getInstance()
-    private val ref = database.getReference("fireworks")
+    private val ref = database.reference.child("fireworks")
     private val fireworks = ArrayList<Firework>()
 
     init {
-        ref.addListenerForSingleValueEvent(object : ValueEventListener {
-            override fun onCancelled(error: FirebaseError?) {
-                //test
-            }
+            ref.addListenerForSingleValueEvent( object : ValueEventListener {
 
-            override fun onDataChange(snapshot: DataSnapshot?) {
+            override fun onDataChange(snapshot: DataSnapshot) {
                 val children = snapshot!!.children
                 children.forEach {
                     fireworks.add(it.getValue(Firework::class.java)!!)
                 }
+            }
+            override fun onCancelled(error: DatabaseError) {
+                println("lol")
             }
         })
     }
