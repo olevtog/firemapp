c2ad12f6639191a8ef582f0229f3ee2ebe945ff9
hvalette <zongo512 @gmail.com>
2019-02-14 16:48:40 +0100

    firebase link better + getAll fireworks

--- app/src/main/java/com/valette/defossez/firemapp/controller/FireworksController.kt
+++ app/src/main/java/com/valette/defossez/firemapp/controller/FireworksController.kt
@@ -2,11 +2,32 @@ package com.valette.defossez.firemapp.controller
 
 import com.google.firebase.database.FirebaseDatabase
 import com.valette.defossez.firemapp.entity.Firework
+import com.google.firebase.FirebaseError
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.ValueEventListener
+
+
 
 class FireworksController {
 
     private val database = FirebaseDatabase.getInstance()
     private val ref = database.getReference("fireworks")
+    private val fireworks = ArrayList<Firework>()
+
+    init {
+        ref.addListenerForSingleValueEvent(object : ValueEventListener {
+            override fun onCancelled(error: FirebaseError?) {
+                //test
+            }
+
+            override fun onDataChange(snapshot: DataSnapshot?) {
+                val children = snapshot!!.children
+                children.forEach {
+                    fireworks.add(it.getValue(Firework::class.java)!!)
+                }
+            }
+        })
+    }
 
     fun create(firework: Firework){
         val id = ref.push().key
@@ -16,5 +37,9 @@ class FireworksController {
 
     fun getById(id: String){
 
+    }
+
+    fun getAll() : ArrayList<Firework>{
+        return fireworks
     }
 }
