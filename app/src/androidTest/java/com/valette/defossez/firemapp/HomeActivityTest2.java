18b0112fd38fcbcb048a8c05a7610fcbde7aa60b
gautier <gautier-59@hotmail.fr>
2019-03-17 22:33:19 +0100

    tests home activity

--- app/src/androidTest/java/com/valette/defossez/firemapp/HomeActivityTest2.java
+++ app/src/androidTest/java/com/valette/defossez/firemapp/HomeActivityTest2.java
@@ -3,7 +3,7 @@ package com.valette.defossez.firemapp;
 import android.support.test.filters.LargeTest;
 import android.test.ActivityInstrumentationTestCase2;
 
-import com.valette.defossez.firemapp.activity.FormAddFireworkActivity;
+import com.valette.defossez.firemapp.activity.HomeActivity;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -13,29 +13,26 @@ import static android.support.test.espresso.assertion.ViewAssertions.matches;
 import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
-import static org.hamcrest.core.AllOf.allOf;
 
 @LargeTest
-public class FormActivityTest extends ActivityInstrumentationTestCase2<FormAddFireworkActivity> {
+public class HomeActivityTest2 extends ActivityInstrumentationTestCase2<HomeActivity> {
 
-    public FormActivityTest() {
-        super(FormAddFireworkActivity.class);
+    //on précise que l'on veut tester un MainActivity
+    public HomeActivityTest2() {
+        super(HomeActivity.class);
     }
 
     @Before
     public void setUp() throws Exception {
         super.setUp();
-
         getActivity();
     }
 
     @Test
     public void testContainsIntialViews() {
-        onView(withId(R.id.input_layout_address)).check(matches(isDisplayed()));
-        onView(withId(R.id.inputTime)).check(matches(isDisplayed()));
-        onView(withId(R.id.inputTitle)).check(matches(isDisplayed()));
-        onView(withId(R.id.inputDescription)).check(matches(isDisplayed()));
-        onView(withText("Ajouter l'évènement")).check(matches(isDisplayed()));
+        onView(withId(R.id.search)).check(matches(isDisplayed()));
+        onView(withId(R.id.buttonOpenMenu)).check(matches(isDisplayed()));
+        onView(withId(R.id.boutonLocalisation)).check(matches(isDisplayed()));
     }
 
 }
